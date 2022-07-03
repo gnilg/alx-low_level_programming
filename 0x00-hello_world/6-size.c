b57402a8bce84c36ad3733a7a77d194ec79eb281
gnilg <phogervi03@gmail.com>
2022-07-03 17:16:36 +0000

    first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - debut de la description
+ * Return: toujours retourner la valeur 0
+ */
+int main(void)
+{
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long));
+	printf("Size of a long long int: %lu\n", sizeof(long long));
+	printf("Size of a float:%lu\n", sizeof(float));
+	return (0);
+}
