11b141f3582dc27ab57b229d5185d24164212f0a
gnilg <phogervi03@gmail.com>
2022-10-20 23:12:49 +0000

    debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - tests function that prints if integer is positive or negative
+*Return: 0
+*/
+
+int int main(int argc, char const *argv[])
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+
+	return (0);
+}
