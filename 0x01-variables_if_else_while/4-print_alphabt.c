a499675f4b07b130fa4a0ec224124e48935dcbbe
gnilg <phogervi03@gmail.com>
2022-10-22 16:07:13 +0000

    prints of alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@ char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
     if (ch == 'e')
+    {
+        ch = ch +1;
+    }
+        if (ch == 'q')
         {
             ch = ch +1;
         }
-            if (ch == 'q')
-            {
-                ch = ch +1;
-            }
 putchar(ch);
 
 }
