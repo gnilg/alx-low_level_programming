c5517edd88d2c7031c16a97130e7e2c8284ad5c0
gnilg <phogervi03@gmail.com>
2022-10-22 19:41:23 +0000

    print of numberz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print of alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 122 ; ch >= 97 ; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
