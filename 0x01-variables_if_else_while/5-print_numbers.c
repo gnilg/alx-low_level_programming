767a9b3de3315288a367fbc668a7ad2b0d0020c1
gnilg <phogervi03@gmail.com>
2022-10-22 16:33:42 +0000

    prints of numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print of alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+int nb;
+for (nb = 0 ; nb <= 9 ; nb++)
+printf("%d", nb);
+printf("\n");
+return (0);
+}
