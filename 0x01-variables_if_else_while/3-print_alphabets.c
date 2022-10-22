93f12ce9367f202d6eabd6411f389fa40692fc5f
gnilg <phogervi03@gmail.com>
2022-10-22 15:10:59 +0000

    prints of alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- print of alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
