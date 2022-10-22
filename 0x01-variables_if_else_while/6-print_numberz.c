f9837db353d1c01ccba1e7b06bee42b50677a1bc
gnilg <phogervi03@gmail.com>
2022-10-22 16:39:57 +0000

    prints of numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print of alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+int nb;
+for (nb = 48 ; nb <= 57 ; nb++)
+putchar(nb);
+printf("\n");
+return (0);
+}
