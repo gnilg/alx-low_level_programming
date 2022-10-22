e255375565e87d869381d6ad66a9f95a6bd5a953
gnilg <phogervi03@gmail.com>
2022-10-22 10:24:01 +0000

    this commit is for the print of alphabet task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- print of alphabet
+ * Return: always 0
+ */
+
+int main () {
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+   
+   return(0);
+}
