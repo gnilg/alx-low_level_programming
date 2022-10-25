8c4a51417cc8b52bccd2d3d9a447ecaa7424584b
gnilg <phogervi03@gmail.com>
2022-10-25 01:42:52 +0000

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "tete.h"
 
 /**
  * main - check the code 
--- 0x05-pointers_arrays_strings/source.c
+++ 0x05-pointers_arrays_strings/source.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "tete.h"
 
 void reset_to_98(int *n){
     *n = 98;
