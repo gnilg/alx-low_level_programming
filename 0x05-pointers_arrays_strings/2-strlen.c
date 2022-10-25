e041914d281628dcb725e834b5d8c60d849237ad
gnilg <phogervi03@gmail.com>
2022-10-25 16:59:20 +0000

    strlen task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return length of char
+ * @s : a pointer of strings
+ * return : int the value
+ */
+
+
+int _strlen(char *s)
+{
+*s = strlen(*str);
+}
