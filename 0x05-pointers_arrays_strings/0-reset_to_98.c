c34b62113a27161dde25b4d7a56af2978d65cb7b
gnilg <phogervi03@gmail.com>
2022-10-25 14:11:34 +0000

    review of projet

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * 0-reset_to_98 - take pointer value to reset it to 98
- * *n = pointer
+ * reset_to_98 - take pointer value to reset it to 98
+ * @n = a pointer
  * return :basicaly void
  */
 
