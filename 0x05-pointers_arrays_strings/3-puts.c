991ea99145b6b784f41c167b71cacccac625557d
gnilg <phogervi03@gmail.com>
2023-02-28 01:30:16 +0100

    update puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include "main.h"
 /*
-*
+*main -_puts foncrion
+*return : always 0
 */
+void _puts(char str[]);
 int main(){
+    char chaine[] = "I do not fear computers. I fear the lack of them - Isaac Asimov"; 
 
+    _puts(chaine);
     
+
+
     return (0);
 }
+
+void _puts(char str[]){
+    int i = 0;
+    char s = 'q';
+    while (str[i] != '\0')
+    {
+        s = str[i];
+        _putchar(s);
+        i++;
+    }
+}
+    
+
+int _putchar(char c){
+ return (write(1, &c, 1));
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,10 +6,15 @@
  */
 
 #include <stdlib.h>
+#include <unistd.h>
+
 
 
 void reset_to_98(int *nb);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+
 
 #endif
